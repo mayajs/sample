a58bf8d503b92295280901f8822f1d197007ae40
Mac Ignacio <mack_ignacio@yahoo.com>
2019-08-11 15:21:49 +0800

    Add server.ts

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import { Express } from "express";
+import maya from "@mayajs/core";
+
+class Server {
+  maya: maya;
+
+  constructor() {
+    this.maya = new maya();
+    this.configRoutes();
+  }
+
+  get app(): Express {
+    return this.maya.app;
+  }
+
+  configRoutes(): void {
+    const { route, middlewares, router } = this.maya.routes({
+      controllers: [],
+      middlewares: [],
+      route: "/api/v1",
+    });
+
+    this.app.use(route, middlewares, router);
+  }
+}
+
+export default new Server();
