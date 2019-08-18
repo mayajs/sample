858175e7883f72d393bfc42322a7f9632629983b
Mac Ignacio <mack_ignacio@yahoo.com>
2019-08-18 18:31:44 +0800

    Inject SampleServices

--- src/controllers/sample/sample.controllers.ts
+++ src/controllers/sample/sample.controllers.ts
@@ -1,48 +1,55 @@
 import { Controller, Delete, Get, Patch, Post, Put } from "@mayajs/core";
 import { Request, Response, NextFunction } from "express";
+import { SampleServices } from "./sample.service";
 
 @Controller({
   model: "./sample.model",
   route: "/sample",
 })
 export class SampleController {
+  // Inject SampleServices
+  constructor(private services: SampleServices) {}
+
   // This is a GET request equal to "/sample"
-  @Get({ path: "/", validations: [] })
+  @Get({ path: "/", middlewares: [] })
   get(req: Request, res: Response, next: NextFunction): void {
+    // Use a function on SampleService
+    this.services.getSamples();
+
     // Do some GET stuff here
     res.send("This is a GET request");
   }
 
   // This is a GET request equal to "/sample/:id"
-  @Get({ path: "/:id", validations: [] })
+  @Get({ path: "/:id", middlewares: [] })
   getId(req: Request, res: Response, next: NextFunction): void {
     // Do some GET stuff here
     res.send("This is a GET with id request");
   }
 
   // This is a POST request equal to "/sample/:id/:name"
-  @Post({ path: "/:id/:name", validations: [] })
+  @Post({ path: "/:id/:name", middlewares: [] })
   post(req: Request, res: Response, next: NextFunction): void {
     // Do some POST stuff here
     res.send("This is a POST request");
   }
 
   // This is a PATCH request equal to "/sample/:id/custom-path"
-  @Patch({ path: "/:id/custom-path", validations: [] })
+  @Patch({ path: "/:id/custom-path", middlewares: [] })
   patch(req: Request, res: Response, next: NextFunction): void {
     // Do some PATCH stuff here
     res.send("This is a PATCH request");
   }
 
   // This is a PUT request equal to "/sample/:id"
-  @Put({ path: "/:id", validations: [] })
+  @Put({ path: "/:id", middlewares: [] })
   put(req: Request, res: Response, next: NextFunction): void {
     // Do some PUT stuff here
     res.send("This is a PUT request");
   }
 
   // This is a DELETE request equal to "/sample/:id"
-  @Delete({ path: "/:id", validations: [] })
+  @Delete({ path: "/:id", middlewares: [] })
   delete(req: Request, res: Response, next: NextFunction): void {
     // Do some DELETE stuff here
     res.send("This is a DELETE request");
