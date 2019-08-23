a4194186285c18032444fdea9060b8684ebf1ccb
Mac Ignacio <mack_ignacio@yahoo.com>
2019-08-23 10:22:13 +0800

    Replace mongoConnection by database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { App } from "@mayajs/core";
+import { Mongo } from "@mayajs/mongo";
 import { routes } from "./app.routing.module";
 
 @App({
   cors: true, // Default false
   logs: process.env.NODE_ENV, // Default false
-  mongoConnection: {
+  database: Mongo({
     connectionString: "your-connection-string-here",
     options: { useCreateIndex: true, useNewUrlParser: true, useFindAndModify: false },
-  },
+  }),
   port: Number(process.env.PORT), // Default port:3333
   routes,
 })
