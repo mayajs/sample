450e14324c54daac14a17248025d458c22fb66b8
Mac Ignacio <mack_ignacio@yahoo.com>
2019-08-23 12:54:14 +0800

    Add dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { App } from "@mayajs/core";
 import { Mongo } from "@mayajs/mongo";
 import { routes } from "./app.routing.module";
+import { config } from "dotenv";
+config();
 
 @App({
   cors: true, // Default false
   logs: process.env.NODE_ENV, // Default false
   database: Mongo({
-    connectionString: "your-connection-string-here",
+    connectionString: process.env.MONGO_CONNECTION_URL || "your-connection-string-here",
     options: { useCreateIndex: true, useNewUrlParser: true, useFindAndModify: false },
   }),
   port: Number(process.env.PORT), // Default port:3333
