0a9c5d5a57ece1388a08c1f31fe14a53d4d73032
Mac Ignacio <mack_ignacio@yahoo.com>
2020-07-26 20:02:29 +0800

    FEAT: Remove Mayajs files

--- src/app.routing.module.ts
+++
@@ -1,9 +0,0 @@
-import { SampleController } from "./controllers/sample/sample.controller";
-
-export const routes = [
-  {
-    controllers: [SampleController],
-    middlewares: [],
-    path: "",
-  },
-];
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import { AppModule } from "./app.module";
-import { MayaJS } from "@mayajs/core";
-
-const server = new MayaJS(AppModule);
-const prod = process.env.NODE_ENV === "production";
-server.prodMode(prod).start();
