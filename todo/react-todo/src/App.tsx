7ebf801fac25a6493f20680343bd6f676dba3b08
Mac Ignacio <mack_ignacio@yahoo.com>
2020-07-29 00:12:18 +0800

    FEAT: Add header in app

--- todo/react-todo/src/App.tsx
+++ todo/react-todo/src/App.tsx
@@ -1,8 +1,15 @@
 import React from "react";
+import Header from "./components/utility/Header";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <Header />
+      </div>
+    </div>
+  );
 }
 
 export default App;
