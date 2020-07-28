1eafa2c3a2a161dc4804f4f4def593711faeb3b3
Mac Ignacio <mack_ignacio@yahoo.com>
2020-07-29 02:22:11 +0800

    FEAT: Add IPropsTodoAdds in todo add component

--- todo/react-todo/src/components/todo/TodoAdd.tsx
+++ todo/react-todo/src/components/todo/TodoAdd.tsx
@@ -1,17 +1,24 @@
-import React, { Component, ChangeEvent } from "react";
+import React, { Component, ChangeEvent, FormEvent } from "react";
+import { IPropsTodoAdd } from "../interfaces";
 
-export default class TodoAdd extends Component {
+export default class TodoAdd extends Component<IPropsTodoAdd> {
   state = {
     title: "",
   };
 
   onChange = (event: ChangeEvent<HTMLInputElement>) => this.setState({ [event.target.name]: event.target.value });
 
+  onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    this.props.addItem(this.state.title);
+    this.setState({ title: "" });
+  };
+
   btnStyle = { flex: "2", backgroundColor: "#333", color: "white", cursor: "pointer" };
 
   render() {
     return (
-      <form style={{ display: "flex" }}>
+      <form style={{ display: "flex" }} onSubmit={this.onSubmit}>
         <input
           type="text"
           name="title"
