785be61b80b1c5c1e65976fce28f297b044c1f56
Mac Ignacio <mack_ignacio@yahoo.com>
2020-07-29 02:13:35 +0800

    FEAT: Add input text to get todo item title

--- todo/react-todo/src/components/todo/TodoAdd.tsx
+++ todo/react-todo/src/components/todo/TodoAdd.tsx
@@ -1,10 +1,26 @@
-import React, { Component } from "react";
+import React, { Component, ChangeEvent } from "react";
 
 export default class TodoAdd extends Component {
+  state = {
+    title: "",
+  };
+
+  onChange = (event: ChangeEvent<HTMLInputElement>) => this.setState({ [event.target.name]: event.target.value });
+
+  btnStyle = { flex: "2", backgroundColor: "#333", color: "white", cursor: "pointer" };
+
   render() {
     return (
-      <form>
-        <input type="submit" value="Submit" className="btn" style={{ flex: "1" }} />
+      <form style={{ display: "flex" }}>
+        <input
+          type="text"
+          name="title"
+          style={{ flex: "6", padding: ".5rem" }}
+          placeholder="Enter Todo ..."
+          value={this.state.title}
+          onChange={this.onChange}
+        />
+        <input type="submit" value="Submit" className="btn" style={this.btnStyle} />
       </form>
     );
   }
