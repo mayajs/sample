67b8b86e6cd5d4259c4e9e4047ca16594b3b171a
Mac Ignacio <mack_ignacio@yahoo.com>
2020-07-28 23:11:19 +0800

    FIX: Todos schema

--- todo/todo-api/src/controllers/todos/todos.model.ts
+++ todo/todo-api/src/controllers/todos/todos.model.ts
@@ -1,7 +1,12 @@
 import { MongoSchema, MongoModel } from "@mayajs/mongo";
 
 const schema = MongoSchema({
-  description: {
+  title: {
+    required: [true, "Title is required!"],
+    type: String,
+  },
+  completed: {
+    required: [true, "Completed is required!"],
     type: String,
   },
 });
