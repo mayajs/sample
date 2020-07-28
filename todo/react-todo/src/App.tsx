336266c512fcba22b6c4baf291512af4d6afc30f
Mac Ignacio <mack_ignacio@yahoo.com>
2020-07-29 02:57:23 +0800

    FEAT: Add IPropsApp as a props interface for app component

--- todo/react-todo/src/App.tsx
+++ todo/react-todo/src/App.tsx
@@ -2,37 +2,33 @@ import React, { Component } from "react";
 import Header from "./components/utility/Header";
 import TodoList from "./components/todo/TodoList";
 import TodoAdd from "./components/todo/TodoAdd";
-import { ITodo } from "./components/interfaces";
+import { ITodo, IPropsApp } from "./components/interfaces";
 import axios from "axios";
 import "./App.css";
 
-const APIR_URL = "http://localhost:3333/todos";
+const API_URL = "http://localhost:3333/todos";
 
-class App extends Component<{}, { list: ITodo[] }> {
-  state = {
-    list: [
-      {
-        id: "1",
-        title: "test",
-        completed: false,
-      },
-    ],
-  };
+class App extends Component<{}, IPropsApp> {
+  state: IPropsApp = { list: [] };
+
+  componentDidMount() {
+    axios.get(`${API_URL}`).then((res) => this.setState({ list: res.data }));
+  }
 
   // Toggle Complete
   toggleComplete = (id: string) => this.setState({ list: this.state.list.map(this.mapItem(id)) });
 
   // Map List Items
-  mapItem = (id: string) => (item: ITodo) => ({ ...item, completed: item.id === id ? !item.completed : item.completed });
+  mapItem = (id: string) => (item: ITodo) => ({ ...item, completed: item._id === id ? !item.completed : item.completed });
 
   // Delete Todo Item
   deleteItem = (id: string) => {
-    axios.delete(`${APIR_URL}/${id}`).then((res) => this.setState({ list: [...this.state.list.filter((item) => item.id !== id)] }));
+    axios.delete(`${API_URL}/${id}`).then((res) => this.setState({ list: [...this.state.list.filter((item) => item._id !== id)] }));
   };
 
   // Add Todo Item
   addItem = (title: string) => {
-    axios.post(APIR_URL, { title, completed: false }).then((res) => this.setState({ list: [...this.state.list, res.data] }));
+    axios.post(API_URL, { title, completed: false }).then((res) => this.setState({ list: [...this.state.list, res.data] }));
   };
 
   actions = {
