dc6e04c421930f556ca657c325d1f106299f22ca
Jc Mandawe <jcpmandawe@gmail.com>
2020-07-30 00:50:53 +0800

    FEAT: Add todos

---
+++ todo/vue-todo/src/components/Todos.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    {{ info }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import axios from "axios";
+
+@Component({})
+export default class Todos extends Vue {
+  @Prop() private msg!: string;
+  private info!: any;
+  data() {
+    return {
+      info: null,
+    };
+  }
+  mounted() {
+    axios
+      .get("http://localhost:3333/todos")
+      .then((response) => (this.info = response.data))
+      .catch((error) => console.log(error));
+  }
+}
+</script>
+
+<style scoped></style>
