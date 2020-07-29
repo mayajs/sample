2a9f5d2bc76e81a1088ac909249da692f668dc45
Jc Mandawe <jcpmandawe@gmail.com>
2020-07-30 02:37:30 +0800

    FEAT: Add todo update form

--- todo/vue-todo/src/components/Todos.vue
+++ todo/vue-todo/src/components/Todos.vue
@@ -2,13 +2,21 @@
   <div>
     {{ msg }}
     <ul>
-      <li v-for="todo in todos" :key="todo._id">{{ todo.title }} <span>edit</span> <button v-on:click="deleteTodo(todo._id)">remove</button></li>
+      <li v-for="todo in todos" :key="todo._id">
+        {{ todo.title }} <button v-on:click="editTodo(todo._id, todo.title)">edit</button> <button v-on:click="deleteTodo(todo._id)">remove</button>
+      </li>
     </ul>
-    <form method="post" @submit.prevent="postTodo">
+
+    <form @submit.prevent="postTodo" v-if="!this.id">
       <input v-model="title" placeholder="title" />
       <button>Submit</button>
     </form>
-    <p>Title is: {{ title }}</p>
+
+    <form @submit.prevent="patchTodo" v-if="this.id">
+      <input v-model="id" placeholder="id" hidden />
+      <input v-model="title" placeholder="title" />
+      <button>Update</button>
+    </form>
   </div>
 </template>
 
@@ -25,6 +33,7 @@ export interface ITodo {
 @Component({})
 export default class Todos extends Vue {
   @Prop() private msg!: string;
+  private id!: string;
   private title!: string;
   private completed!: boolean;
   private todos!: ITodo[];
@@ -33,6 +42,7 @@ export default class Todos extends Vue {
   data() {
     return {
       todos: null,
+      id: "",
       title: "",
       completed: false,
     };
@@ -59,14 +69,20 @@ export default class Todos extends Vue {
       .catch((error) => console.log(error));
   }
 
-  patchTodo(id: string) {
+  patchTodo() {
     axios
-      .patch<ITodo>(`${this.url}/${id}`, { title: this.title, completed: this.completed })
+      .patch<ITodo>(`${this.url}/${this.id}`, { title: this.title, completed: this.completed })
       .then(() => {
+        this.id = "";
         this.title = "";
         this.getTodos();
       })
       .catch((error) => console.log(error));
+  }
+
+  editTodo(id: string, title: string) {
+    this.id = id;
+    this.title = title;
   }
 
   deleteTodo(id: string) {
