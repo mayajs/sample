74d5496a1bb1788694c2ee583737545fbce0571a
Jc Mandawe <jcpmandawe@gmail.com>
2020-07-29 14:47:27 +0800

    FEAT: Update getAll function

--- todo/flutter-todo/lib/database.service.dart
+++ todo/flutter-todo/lib/database.service.dart
@@ -1,12 +1,25 @@
 import 'dart:convert' as convert;
+import 'package:flutter_todo/todo.model.dart';
+import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
 
 class DatabaseService {
   final String url = 'http://10.0.2.2:3333/todos';
   final Map<String, String> headers = {'Content-type': 'application/json'};
 
-  getAll(String url, {Map<String, String> headers}) async {
-    return await http.get(url, headers: headers);
+  Future<List<TodoModel>> getAll() async {
+    http.Response res = await http.get(url, headers: headers);
+
+    if (res.statusCode == 200) {
+      List<dynamic> body = convert.jsonDecode(res.body);
+
+      List<TodoModel> todos =
+          body.map((dynamic item) => TodoModel.fromJson(item)).toList();
+
+      return todos;
+    } else {
+      throw "TODOS not found!";
+    }
   }
 
   getById(String url, String id, {Map<String, String> headers}) async {
