cd008f232c462b2e88a88efe8bb08c72a68ac17b
Jc Mandawe <jcpmandawe@gmail.com>
2020-07-29 13:35:41 +0800

    FEAT: Add delete function

--- todo/flutter-todo/lib/database.dart
+++ todo/flutter-todo/lib/database.dart
@@ -2,7 +2,7 @@ import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
 
 class Database {
-  String url = 'http://10.0.2.2:3333/todos';
+  final String url = 'http://10.0.2.2:3333/todos';
   final Map<String, String> headers = {'Content-type': 'application/json'};
 
   getAll(String url, {Map<String, String> headers}) async {
@@ -13,11 +13,19 @@ class Database {
     return await http.get(url + id, headers: headers);
   }
 
-  post(String url, String id, {Map<String, String> headers, Map<String, dynamic> data}) async {
-    return await http.post(url, headers: headers, body: convert.jsonEncode(data));
+  post(String url, String id,
+      {Map<String, String> headers, Map<String, dynamic> data}) async {
+    return await http.post(url,
+        headers: headers, body: convert.jsonEncode(data));
   }
 
-  patch(String url, String id, {Map<String, String> headers, Map<String, dynamic> data}) async {
-    return await http.patch(url + id, headers: headers, body: convert.jsonEncode(data));
+  patch(String url, String id,
+      {Map<String, String> headers, Map<String, dynamic> data}) async {
+    return await http.patch(url + id,
+        headers: headers, body: convert.jsonEncode(data));
+  }
+
+  delete(String url, String id, {Map<String, String> headers}) async {
+    return await http.delete(url + id, headers: headers);
   }
 }
