4e66de39a83ffa6dddf1b8124ff0271c22598616
Steven Alfred Santillan <tiben4001@gmail.com>
2020-07-30 19:04:50 +0800

    FEAT: Add import FormsModule

--- todo/angular/src/app/app.module.ts
+++ todo/angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -8,7 +9,7 @@ import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
 
 @NgModule({
   declarations: [AppComponent, TodoComponent, NavBarComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
