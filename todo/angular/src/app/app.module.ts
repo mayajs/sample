a7c908ccc2e9dd2ce64c99e95a3ed15013690391
Steven Alfred Santillan <tiben4001@gmail.com>
2020-07-30 19:04:21 +0800

    FEAT: Add import NavBarComponent

--- todo/angular/src/app/app.module.ts
+++ todo/angular/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { TodoComponent } from "./components/todo/todo.component";
+import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
 
 @NgModule({
-  declarations: [AppComponent, TodoComponent],
+  declarations: [AppComponent, TodoComponent, NavBarComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
