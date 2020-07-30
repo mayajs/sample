8ee268317c7e1df60f4e1c6fa546cd677c0a243c
Steven Alfred Santillan <tiben4001@gmail.com>
2020-07-30 21:35:58 +0800

    FEAT: Add import HttpClientModule to app module

--- todo/angular/src/app/app.module.ts
+++ todo/angular/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -9,7 +10,7 @@ import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
 
 @NgModule({
   declarations: [AppComponent, TodoComponent, NavBarComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
