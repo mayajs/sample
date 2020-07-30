45c7bd421cc60f2527500c6aa6f639754cb8d242
Mac Ignacio <mack_ignacio@yahoo.com>
2020-07-31 03:05:09 +0800

    FEAT: Add todo list array

--- todo/svelte-todo/src/App.svelte
+++ todo/svelte-todo/src/App.svelte
@@ -1,11 +1,20 @@
 <script>
-  export let name;
+  import { Container } from "sveltestrap";
+  let list = [
+    { title: "Sleeping", completed: false, id: 1 },
+    { title: "Walking", completed: false, id: 2 },
+    { title: "Eating", completed: false, id: 3 },
+  ];
 </script>
 
 <style>
 
 </style>
 
-<main>
-  <h1>Hello {name}!</h1>
-</main>
+<Container>
+  {#each list as item (item.id)}
+    <div>
+      <h4>{item.title}</h4>
+    </div>
+  {/each}
+</Container>
