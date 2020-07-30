148dae979dc461ab86a448d668e1d44acec7869d
Renellie Alfred Maglasang <technurvuz@gmail.com>
2020-07-30 14:38:28 +0800

    FEAT: Add database string connection

--- crud/mongodb/src/databases/mongo.ts
+++ crud/mongodb/src/databases/mongo.ts
@@ -1,10 +1,10 @@
 import { Mongo } from "@mayajs/mongo";
-import sample from "../controllers/sample/sample.model";
+import users from "../controllers/users/users.model";
 
 export = Mongo({
-  name: "your-db-name-here",
-  connectionString: "your-connection-string-here",
+  name: "test",
+  connectionString: "mongodb+srv://muriusbane:root@cluster0.7ispv.mongodb.net/test?retryWrites=true&w=majority",
   schemas: [
-    sample, // Add Mongoose Schema here
+    users, 
   ],
 });
