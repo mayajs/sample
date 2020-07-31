89ba5f38fd3e57e99dc260a352495e0c01e3d007
Steven Alfred Santillan <tiben4001@gmail.com>
2020-07-31 19:27:18 +0800

    CHORE: Remove todo list logic

--- todo/angular/src/app/app.component.ts
+++ todo/angular/src/app/app.component.ts
@@ -1,15 +1,8 @@
-import { Component, NgModule } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  todoList = [];
-  todo = "";
-
-  addTodo(): void {
-    this.todoList.push(this.todo);
-  }
-}
+export class AppComponent {}
