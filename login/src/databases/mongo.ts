dec5e2bf72e58d47c2be167129be60920eb89192
Carebeyond <rhonumahon@gmail.com>
2020-07-31 22:58:41 +0800

    CHORE: Change connection string to env.MONGO_URL

--- login/src/databases/mongo.ts
+++ login/src/databases/mongo.ts
@@ -1,9 +1,10 @@
 import { Mongo } from "@mayajs/mongo";
 import sample from "../controllers/login/login.model";
+import env from "../environments";
 
 export = Mongo({
   name: "your-db-name-here",
-  connectionString: "your-connection-string-here",
+  connectionString: env.MONGO_URL,
   schemas: [
     sample, // Add Mongoose Schema here
   ],
