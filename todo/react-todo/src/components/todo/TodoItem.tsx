7df9c744520af874faf1d87804348031292ad459
Mac Ignacio <mack_ignacio@yahoo.com>
2020-08-01 02:32:59 +0800

    FEAT: Add onEdit in on click edit

--- todo/react-todo/src/components/todo/TodoItem.tsx
+++ todo/react-todo/src/components/todo/TodoItem.tsx
@@ -1,4 +1,4 @@
-import React, { Component, CSSProperties } from "react";
+import React, { Component } from "react";
 import { IPropsTodoItem } from "../interfaces";
 
 export default class TodoItem extends Component<IPropsTodoItem> {
@@ -9,13 +9,13 @@ export default class TodoItem extends Component<IPropsTodoItem> {
   };
 
   render() {
-    const { toggleComplete, deleteItem } = this.props.actions;
+    const { toggleComplete, deleteItem, onEdit } = this.props.actions;
     const { _id, title, completed } = this.props.todo;
     return (
       <li className="list-group-item d-flex justify-content-between align-items-center" style={this.setStyle(completed)}>
         <input className="form-check-input mr-1" type="checkbox" defaultChecked={completed} onChange={toggleComplete.bind(this, _id)} />
         <span className="flex-grow-1 ml-1">{title}</span>
-        <button className="btn btn-action btn-info badge mx-1">
+        <button className="btn btn-action btn-info badge mx-1" onClick={onEdit.bind(this, { _id, title, completed })}>
           <span className="fas fa-edit"></span>
         </button>
 
