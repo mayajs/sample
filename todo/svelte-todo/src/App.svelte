8fba5fc578537d23589733b692e7557df3cfbed6
Mac Ignacio <mack_ignacio@yahoo.com>
2020-07-31 12:33:41 +0800

    FEAT: Add no todo item message when list is empty

--- todo/svelte-todo/src/App.svelte
+++ todo/svelte-todo/src/App.svelte
@@ -3,9 +3,9 @@
 
   let title = "";
   let list = [
-    { title: "Sleeping", completed: false, id: 1 },
+    /*  { title: "Sleeping", completed: false, id: 1 },
     { title: "Walking", completed: false, id: 2 },
-    { title: "Eating", completed: false, id: 3 },
+    { title: "Eating", completed: false, id: 3 }, */
   ];
 
   function onSubmit(event) {
@@ -43,6 +43,10 @@
         <Button color="danger" on:click={() => onDelete(item.id)}>X</Button>
       </Col>
     </Row>
+  {:else}
+    <Col class="text-center">
+      <h4>There are no todo item to show...</h4>
+    </Col>
   {/each}
   <Form class="px-3 mt-2" on:submit={onSubmit}>
     <InputGroup>
