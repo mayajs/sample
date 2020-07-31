8a6ba28f54a1900b4914006e5cfed9bde95864e5
Carebeyond <rhonumahon@gmail.com>
2020-07-31 22:58:41 +0800

    FEAT: Add ILogin and ILoginModel interfaces

--- login/src/controllers/login/login.service.ts
+++ login/src/controllers/login/login.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from "@mayajs/core";
 import { Models } from "@mayajs/mongo";
 import { PaginateModel } from "mongoose";
+import { Document } from "mongoose";
+ interface ILogin {
+  email: string;
+  password: string;
+}
+
+ interface ILoginModel extends ILogin , Document {
+  comparePassword: (password: string) => boolean;
+}
 
 @Injectable()
 export class LoginServices {
-  @Models("login") model!: PaginateModel<any>;
+  @Models("login") model!: PaginateModel<ILoginModel>;
 
   constructor() {}
 
