ac545d68581b33ea7351f26203d8979e3f9a3a17
Mac Ignacio <mack_ignacio@yahoo.com>
2020-08-05 15:23:16 +0800

    FEAT: Replace mongo with sql

---
+++ crud/sql/src/databases/sql.ts
@@ -0,0 +1,18 @@
+import { Sql } from "@mayajs/sql";
+import sample from "../controllers/user/user.model";
+
+export = Sql({
+  name: "sample",
+  options: {
+    database: "test",
+    username: "root",
+    password: "",
+    options: {
+      host: "localhost",
+      dialect: "mysql",
+    },
+  },
+  schemas: [
+    sample, // Add Mongoose Schema here
+  ],
+});
